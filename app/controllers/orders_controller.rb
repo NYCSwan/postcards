18247b6978abfb7a2b0038d75643bb40fec8ee1c
NYCSwan <megan.swanby@gmail.com>
2017-02-23 11:53:40 -0500

    Add payment and confirmation methods to order with basic forms and information persisting to db with cc, user info, etc. not secure, no error handling

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,22 @@ class OrdersController < ApplicationController
 	    if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
 	      redirect_to checkout_payment_path
 	    end
+	   else 
+	   	redirect_to checkout_path, :notice => "You forgot something."
 	  end
+	end
+
+	def payment
+	  if request.post?
+	    redirect_to checkout_confirmation_path
+	  end
+	end
+
+	def confirmation
+		if request.post?
+			current_order.confirm!
+			session[:order_id] = nil
+			redirect_to root_path, :notice => "Order has been placed! Not technically tho.. this is where the cc gateway comes into play"
+		end
 	end
 end
